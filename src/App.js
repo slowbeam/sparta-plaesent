791a7e726ea34103e590c480747bc3af196c303c
Sandy <sedwardscode@gmail.com>
2018-11-26 18:14:24 -0500

    minor font styling for overlay text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,11 @@ class App extends Component {
               <br/>
               <h1>DVF Secret Agent Part 2</h1>
               <h3>October 15, 2017</h3>
-              <p>Join us for a private screening of DVF Secret Agent Part 2</p>
+              <p>
+                Join us for a private screening of DVF Secret Agent Part 2
+                <br/>
+                with our special guest, director Peter Lindberg
+              </p>
             </div>
           </div>
           <ReactResponsiveCarousel />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,6 @@ const NavBar = props => (
   <div className="navbar">
     <nav className="navbar-navigation">
       <div className="navbar-logo"></div>
-      <div className="spacer"></div>
       <div className="navigation-items">
         <div>212.555.5555</div>
         <div>LOGIN</div>
