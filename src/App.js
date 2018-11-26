6da0b5ec3b67b5ec3ebe41253f4eba9c20d8badf
Sandy <sedwardscode@gmail.com>
2018-11-26 17:25:43 -0500

    made max width for carousel navbar and overlay text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,19 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
-        <ReactResponsiveCarousel />
+        <div className="carousel-container">
+          <div className="overlay">
+            <div className="overlay-text">
+              <h1>Screening:</h1>
+              <br/>
+              <h1>DVF Secret Agent Part 2</h1>
+              <h3>October 15, 2017</h3>
+              <p>Join us for a private screening of DVF Secret Agent Part 2</p>
+            </div>
+          </div>
+          <ReactResponsiveCarousel />
+        </div>
+
       </div>
     );
   }
