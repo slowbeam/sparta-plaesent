69f1afcbe5a3f687b1d293b7c0c75241534c121c
Sandy <sedwardscode@gmail.com>
2018-11-26 12:34:13 -0500

    created basic navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = props => (
+  <header className="navbar">
+    <nav className="navbar-navigation">
+      <div className="navbar-logo"></div>
+      <div className="spacer"></div>
+      <div className="navigation-items">
+        <div>212.555.5555</div>
+        <div>LOGIN</div>
+        <div>Hamburger</div>
+      </div>
+
+    </nav>
+  </header>
+);
+
+export default NavBar;
